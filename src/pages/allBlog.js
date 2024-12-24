2c2e3b5bc128bafb4e280b5dd93b4d59dec5ba65
Tuul10 <etuul186@gmial.com>
2024-12-24 17:00:54 +0800

    load more zassn

--- src/pages/allBlog.js
+++ src/pages/allBlog.js
@@ -9,9 +9,7 @@ const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 const AllBlog = () => {
   const url = "https://dev.to/api/articles";
-  const [load, setLoad] = useState(9);
   const { data: blogs = {}, error, isLoading } = useSWR(url, fetcher);
-  console.log(blogs);
 
   if (isLoading) {
     return <p>...loading</p>;
@@ -20,12 +18,6 @@ const AllBlog = () => {
     return <p>...oh sorry error</p>;
   }
 
-  const posts = blogs.slice(0, load);
-
-  const loadMore = () => {
-    setLoad((prev) => prev + 9);
-  };
-
   return (
     <div>
       <div className="max-w-[1230px] mx-auto">
@@ -33,16 +25,8 @@ const AllBlog = () => {
       </div>
       <div className="max-w-[1230px] mx-auto">
         <div className=" gap-4">
-          <BigBlog filteredBlogs={posts} />
+          <BigBlog filteredBlogs={blogs} />
         </div>
-      </div>
-      <div className="flex justify-center items-center max-w-[1230px] mx-auto mt-4 mb-4">
-        <button
-          onClick={loadMore}
-          className="py-3 px-5 flex justify-center items-center w-fit h-10 rounded-md border border-s-gray-400"
-        >
-          Load more
-        </button>
       </div>
     </div>
   );
